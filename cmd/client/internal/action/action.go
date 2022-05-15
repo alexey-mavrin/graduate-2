8c6fc783291eb6fd06ff4116929169fbd2b7e61c
Alexey Mavrin <amavrin@gmail.com>
2022-05-15 17:41:38 +0300

    cache: clean and sync

--- cmd/client/internal/action/action.go
+++ cmd/client/internal/action/action.go
@@ -33,11 +33,45 @@ func actUser(subop config.OpSubtype, user common.User) error {
 	return nil
 }
 
+func actCache(subop config.OpSubtype) error {
+	clnt := client.NewClient(config.Cfg.ServerAddr,
+		config.Cfg.UserName,
+		config.Cfg.Password,
+		config.Cfg.CacheFile,
+		config.Cfg.HTTPSInsecure,
+	)
+	switch subop {
+	case config.OpSubtypeCacheClean:
+		err := clnt.CleanCache()
+		if err != nil {
+			return err
+		}
+		log.Println("cache is cleaned")
+	case config.OpSubtypeCacheSync:
+		if err := clnt.SyncCacheByType(common.AccountRecord); err != nil {
+			return err
+		}
+		if err := clnt.SyncCacheByType(common.NoteRecord); err != nil {
+			return err
+		}
+		if err := clnt.SyncCacheByType(common.CardRecord); err != nil {
+			return err
+		}
+		if err := clnt.SyncCacheByType(common.BinaryRecord); err != nil {
+			return err
+		}
+		log.Println("cache is synchronized")
+	}
+	return nil
+}
+
 // ChooseAct performs client actions
 func ChooseAct() error {
 	switch config.Op.Op {
 	case config.OpTypeUser:
 		return actUser(config.Op.Subop, config.Op.User)
+	case config.OpTypeCache:
+		return actCache(config.Op.Subop)
 	case config.OpTypeAccount:
 		return actRecord(config.Op.Subop, config.Op.Account)
 	case config.OpTypeNote:
